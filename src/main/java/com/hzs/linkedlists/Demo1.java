d624890f550ee7fb181556d92bfeacd42b0064e2
DennisHe <1569929533@qq.com>
2019-09-01 16:58:17 +0800

    修改打印链表的方法

---
+++ src/main/java/com/hzs/linkedlists/Demo1.java
@@ -0,0 +1,22 @@
+package com.hzs.linkedlists;
+
+import java.util.List;
+
+/**
+ * @author hezhensheng
+ * @version 1.0
+ * @description Demo1
+ * @date 2019/9/1 16:55
+ */
+
+public class Demo1 {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.add(new ListNode(2));
+        head.add(new ListNode(3));
+        head.add(new ListNode(4));
+
+        head.print();
+
+    }
+}
