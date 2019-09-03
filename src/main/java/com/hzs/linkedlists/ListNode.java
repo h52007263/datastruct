9164f42668388e573516f23d612045778d24a617
DennisHe <1569929533@qq.com>
2019-09-03 17:20:45 +0800

    删除链表节点

--- src/main/java/com/hzs/linkedlists/ListNode.java
+++ src/main/java/com/hzs/linkedlists/ListNode.java
@@ -15,6 +15,10 @@ public class ListNode {
         this.val = val;
     }
 
+    /**
+     * 链表尾部增加一个节点
+     * @param node
+     */
     public void add(ListNode node){
         if(node == null)
             throw new NullPointerException("新增节点是空的");
@@ -27,6 +31,9 @@ public class ListNode {
         node.next = null;
     }
 
+    /**
+     * 打印链表节点的值
+     */
     void print(){
         if(this.val == 0)
             throw new IllegalArgumentException("val 不可以为0");
@@ -38,6 +45,11 @@ public class ListNode {
         }
     }
 
+    /**
+     * 创建length个节点
+     * @param length
+     * @return 头节点
+     */
     public ListNode createTestList(int length){
         if( this.val != 1)
             this.val = 1;
@@ -74,6 +86,37 @@ public class ListNode {
             p = p.next;
         }
         return len;
+    }
+
+    // 通过节点删除
+    public ListNode remove(ListNode removeNode) {
+        if (removeNode == null)
+            return null;
+        ListNode p = this;
+        // step1:找到待删除的节点
+        while (p.next != removeNode) {
+            p = p.next;
+        }
+        // step2:开始删除removeNode节点
+        p.next = removeNode.next;
+        removeNode.next = null;
+        return this;
+    }
+
+    // 通过索引删除
+    public ListNode remove(int index) {
+        if (index <= 0 || index > this.size())
+            throw new IllegalArgumentException("非法入参index");
+        ListNode p = this;
+        // step1:找到待删除节点
+        for (int i = 1; i < index - 1; i++) {
+            p = p.next;
+        }
+        // step2:开始删除节点
+        ListNode removeNode = p.next;
+        p.next = p.next.next;
+        removeNode.next = null;
+        return this;
     }
 
     @Override
